11d08247c9c44a3f3861f32727eb14b92c426b99
Patricia bellardine <patriciabellardine@Patricias-MacBook-Pro.local>
2018-05-08 14:52:50 -0700

    add navbar, links and contact

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <div>
+    <h1>About The Great Gatsby</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum veritatis molestiae modi assumenda quis error ipsum quo obcaecati consequatur vel expedita repellat beatae quam labore pariatur, nam impedit tempora ipsam.
+    Alias, facere? Autem tempore delectus magnam? Repellendus alias repudiandae a molestias quas ipsum, cum officiis dolores. Saepe ex ipsum fuga architecto facere alias laudantium natus repellendus? Corporis numquam sint earum.
+    Tempora atque vel sint neque officia fugit quisquam assumenda, animi cum, ducimus quae repellat tenetur voluptas labore nemo nulla porro itaque laboriosam cupiditate! Magni, in soluta? Ab tenetur voluptatum eius.
+    Et explicabo quasi perferendis, quibusdam ea est ratione, non exercitationem voluptatem molestiae nesciunt, cum aliquam iusto pariatur corporis voluptatum? Totam a nemo inventore ex magnam excepturi fugiat velit molestias veniam?
+    Natus, voluptates! Voluptas, cumque a. Ad, dolorum deserunt! Earum, temporibus. Laboriosam quibusdam ab, aliquid a quisquam exercitationem, quasi voluptas fugit dolorum sapiente omnis nesciunt deserunt accusantium dignissimos ratione vitae distinctio?
+    Repudiandae vitae sed doloremque voluptatem sequi, officiis eligendi dolorum odit id soluta maxime est, ullam ex a. Fugit, praesentium dolor minus, tempore nesciunt nemo sint numquam tempora suscipit in cumque!
+    Rerum minus eligendi non quisquam cumque officia asperiores voluptatum eveniet odit exercitationem? Dolores minima sint ipsum maiores, ipsam dignissimos, nam fuga ea laborum et quia quae dolore non error iure.
+    Illum consectetur quibusdam natus incidunt, totam eius ipsa esse fuga perferendis consequuntur, cum hic ex necessitatibus quod provident dolorum reprehenderit ullam quaerat ipsum dicta sed pariatur quo. Optio, possimus culpa?
+    Est illo itaque eaque sint voluptas. Vitae dolorem facere natus officiis cum, vel fugiat nesciunt numquam labore dicta laboriosam, aliquam corrupti ex eum deleniti repellendus! Sapiente amet quisquam cupiditate minus?
+    Eos deleniti molestiae tempore delectus totam est, possimus nisi, error reiciendis quibusdam quae, nesciunt et repellat neque minus quod aliquam adipisci suscipit recusandae reprehenderit dolores ab. Quasi minima officia possimus.</p>
+  </div>
+)
+
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => (
+  <div>
+    <h1>Contact The Great Gatsby</h1>
+    <p>
+      What you think yo!?
+    </p>
+  </div>
+)
+
+export default ContactPage
